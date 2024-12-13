f19b48e61e2ef177d38ff2c960cf8cdaa6fd1da8
ᥴ̶̧̪̤̓͊̌̽̊̎̓͜͝͠ꪮ̵͈̀͐͗͒̽͛͊͝ꪀ̸̧̨̺̮̩̣̤͂̾͜ᦓ̵̠̂̅̈́̊ͅꪮ̶̢͇̟͆͗͆́̌͗͂͒̀͜͝ꪶ̴̨̧̖͍̞̲̖̠̆̽̈́̇̾̍̒̕͠ͅꫀ̵̡̨̧̛͚͕̝̭̦̩̻̈́̚̚ͅ <162306504+cons0le7@users.noreply.github.com>
2024-12-13 21:44:44 +0000

    Add files via upload

--- keep_rotate.sh
+++ keep_rotate.sh
@@ -2,33 +2,28 @@
 
 SCRIPT_NAME="rotation.sh"
 SCRIPT_SOURCE="$(pwd)/$SCRIPT_NAME"
-SCRIPT_DEST="/usr/local/bin/$SCRIPT_NAME"
-SERVICE_FILE="/etc/systemd/system/hyperpixel_rotate.service"
+SCRIPT_DEST="$HOME/.local/bin/$SCRIPT_NAME"
+DESKTOP_FILE="$HOME/.config/autostart/rotation.desktop"
 
 if [[ ! -f $SCRIPT_SOURCE ]]; then
     echo "Source script $SCRIPT_SOURCE does not exist."
     exit 1
 fi
 
-sudo cp "$SCRIPT_SOURCE" "$SCRIPT_DEST"
-sudo chmod +x "$SCRIPT_DEST"
+mkdir -p "$HOME/.local/bin"
 
-cat << EOF | sudo tee "$SERVICE_FILE"
-[Unit]
-Description=My Custom Script Service
-After=network.target
+cp "$SCRIPT_SOURCE" "$SCRIPT_DEST"
+chmod +x "$SCRIPT_DEST"
 
-[Service]
-Type=simple
-ExecStart=$SCRIPT_DEST
-Restart=on-failure
-
-[Install]
-WantedBy=multi-user.target
+cat << EOF > "$DESKTOP_FILE"
+[Desktop Entry]
+Type=Application
+Exec=$SCRIPT_DEST
+Hidden=false
+NoDisplay=false
+X-GNOME-Automatic-Start=true
+X-GNOME-Autostart-enabled=true
+Name=Rotation Script
 EOF
 
-sudo systemctl daemon-reload
-sudo systemctl enable hyperpixel_rotate.service
-sudo systemctl start hyperpixel_rotate.service 
-
-echo "Service for $SCRIPT_NAME has been created and enabled to start at boot."
+echo "The $SCRIPT_NAME has been added to autostart."
