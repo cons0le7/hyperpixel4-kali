6f5311ade8f89d46ed9957436daba11ad002343a
ᥴ̶̧̪̤̓͊̌̽̊̎̓͜͝͠ꪮ̵͈̀͐͗͒̽͛͊͝ꪀ̸̧̨̺̮̩̣̤͂̾͜ᦓ̵̠̂̅̈́̊ͅꪮ̶̢͇̟͆͗͆́̌͗͂͒̀͜͝ꪶ̴̨̧̖͍̞̲̖̠̆̽̈́̇̾̍̒̕͠ͅꫀ̵̡̨̧̛͚͕̝̭̦̩̻̈́̚̚ͅ <162306504+cons0le7@users.noreply.github.com>
2024-12-13 21:23:33 -0700

    Update hyperpixel4-kali.sh

--- hyperpixel4-kali.sh
+++ hyperpixel4-kali.sh
@@ -2,4 +2,4 @@
 sudo apt install python3-dev 
 sudo apt install python3-rpi.gpio 
 FILE="/usr/bin/hyperpixel4-init"
-[ -f "$FILE" ] && sed -i '1s/python/python3/' "$FILE"
+[ -f "$FILE" ] && sudo sed -i '1s/python/python3/' "$FILE"
