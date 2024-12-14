2f1a86e93294dcdf078695e3e90e8f14288ffcd6
ᥴ̶̧̪̤̓͊̌̽̊̎̓͜͝͠ꪮ̵͈̀͐͗͒̽͛͊͝ꪀ̸̧̨̺̮̩̣̤͂̾͜ᦓ̵̠̂̅̈́̊ͅꪮ̶̢͇̟͆͗͆́̌͗͂͒̀͜͝ꪶ̴̨̧̖͍̞̲̖̠̆̽̈́̇̾̍̒̕͠ͅꫀ̵̡̨̧̛͚͕̝̭̦̩̻̈́̚̚ͅ <162306504+cons0le7@users.noreply.github.com>
2024-12-14 06:01:49 +0000

    Add files via upload

--- rotation.sh
+++ rotation.sh
@@ -1,28 +1,66 @@
 
-#Uncomment the 2 lines below your chosen layout.
+# Uncomment the 2 lines below your chosen layout.
 
 
-#LEFT (Landscape - HDMI & power port on bottom)
+#######################
+# Rectangular display #
+#######################
+
+# - LEFT (Landscape - HDMI & power port on bottom)
 
 #DISPLAY=:0.0 xrandr --output DSI-1 --rotate left
 #DISPLAY=:0.0 xinput set-prop "pointer:Goodix Capacitive TouchScreen" "libinput Calibration Matrix" 0 -1 1 1 0 0 0 0 1
 
 
-#RIGHT (Landscape - HDMI & power port on top)
+# - RIGHT (Landscape - HDMI & power port on top)
 
 #DISPLAY=:0.0 xrandr --output DSI-1 --rotate right
 #DISPLAY=:0.0 xinput set-prop "pointer:Goodix Capacitive TouchScreen" "libinput Calibration Matrix" 0 1 0 -1 0 1 0 0 1
 
 
-#NORMAL (Portriat - HDMI & power port to the right)
+# - NORMAL (Portriat - HDMI & power port to the right)
 
 #DISPLAY=:0.0 xrandr --output DSI-1 --rotate normal
 #DISPLAY=:0.0 xinput set-prop "pointer:Goodix Capacitive TouchScreen" "libinput Calibration Matrix" 1 0 0 0 1 0 0 0 1
 
 
-#INVERTED (Portriat - HDMI & power port to the left)
+# - INVERTED (Portriat - HDMI & power port to the left)
 
 #DISPLAY=:0.0 xrandr --output DSI-1 --rotate inverted
 #DISPLAY=:0.0 xinput set-prop "pointer:Goodix Capacitive TouchScreen" "libinput Calibration Matrix" -1 0 1 0 -1 1 0 0 1
+
+
+
+##################
+# Square display #
+##################
+
+# - LEFT (Landscape - HDMI & power port on bottom)
+
+#DISPLAY=:0.0 xrandr --output DSI-1 --rotate left
+#DISPLAY=:0.0 xinput set-prop "pointer:generic ft5x06 (11)" "libinput Calibration Matrix" 0 -1 1 1 0 0 0 0 1
+
+
+# - RIGHT (Landscape - HDMI & power port on top)
+
+#DISPLAY=:0.0 xrandr --output DSI-1 --rotate right
+#DISPLAY=:0.0 xinput set-prop "pointer:generic ft5x06 (11)" "libinput Calibration Matrix" 0 1 0 -1 0 1 0 0 1
+
+
+# - NORMAL (Portriat - HDMI & power port to the right)
+
+#DISPLAY=:0.0 xrandr --output DSI-1 --rotate normal
+#DISPLAY=:0.0 xinput set-prop "pointer:generic ft5x06 (11)" "libinput Calibration Matrix" 1 0 0 0 1 0 0 0 1
+
+
+# - INVERTED (Portriat - HDMI & power port to the left)
+
+#DISPLAY=:0.0 xrandr --output DSI-1 --rotate inverted
+#DISPLAY=:0.0 xinput set-prop "pointer:generic ft5x06 (11)" "libinput Calibration Matrix" -1 0 1 0 -1 1 0 0 1
+
+
+
+
+
 
 
